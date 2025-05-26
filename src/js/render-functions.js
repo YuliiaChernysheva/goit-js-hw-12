b93f9bb63bf48ce03d166a0f993d19859ecba795
Yuliia <chernyshevaulia2905@gmail.com>
2025-05-26 13:44:29 -0600

    added new files

---
+++ src/js/render-functions.js
@@ -0,0 +1,46 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const galleryEl = document.querySelector('.gallery');
+const loaderEl = document.querySelector('.loader');
+
+const lightbox = new SimpleLightbox('.gallery a', {});
+
+export function createGallery(images) {
+  const markup = images
+    .map(
+      image => `
+        <li class="gallery-item">
+          <a class="gallery-link" href="${image.largeImageURL}">
+            <img
+              class="gallery-image"
+              src="${image.webformatURL}"
+              alt="${image.tags}"
+            />
+          </a>
+          <div class="info">
+            <p class="info-item"><b>Likes:</b> ${image.likes}</p>
+            <p class="info-item"><b>Views:</b> ${image.views}</p>
+            <p class="info-item"><b>Comments:</b> ${image.comments}</p>
+            <p class="info-item"><b>Downloads:</b> ${image.downloads}</p>
+          </div>
+        </li>
+      `
+    )
+    .join('');
+
+  galleryEl.insertAdjacentHTML('beforeend', markup);
+  lightbox.refresh();
+}
+
+export function clearGallery() {
+  galleryEl.innerHTML = '';
+}
+
+export function showLoader() {
+  loaderEl.classList.add('visible');
+}
+
+export function hideLoader() {
+  loaderEl.classList.remove('visible');
+}
